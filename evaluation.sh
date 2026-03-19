9ee319db17adc85211033162f6ca606afec75997
Zhimeng Xin <162425451+zhimengXin@users.noreply.github.com>
2026-03-19 19:51:41 +0800

    Update evaluation.sh

--- evaluation.sh
+++ evaluation.sh
@@ -26,7 +26,7 @@ for model in "${model_list[@]}"; do
         --num-gpus 1 \
         --eval-only \
         --config-file configs/${dataset}/vit${model}_shot${shot}_${dataset}_finetune.yaml \
-        MODEL.WEIGHTS weights/prepared_weights/${dataset}_${shot}shot_model_final.pth \pyby
+        MODEL.WEIGHTS weights/prepared_weights/${dataset}_${shot}shot_model_final.pth \
         DE.OFFLINE_RPN_CONFIG configs/RPN/mask_rcnn_R_50_C4_1x_ovd_FSD.yaml \
         OUTPUT_DIR output/vit${model}/${dataset}_${shot}shot/test
 
